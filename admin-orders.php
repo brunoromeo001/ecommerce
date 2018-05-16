f1bebd8ece178e22a7fc0376db30c845eb8ef147
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2018-05-16 14:49:28 -0300

    Error ao atualiazr o status

--- admin-orders.php
+++ admin-orders.php
@@ -3,6 +3,51 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Order;
+use \Hcode\Model\OrderStatus;
+
+$app->get("/admin/orders/:idorder/status", function($idorder){
+	
+	User::verifyLogin();
+	
+	$order = new Order();
+			
+	$order->get((int)$idorder);
+	
+	$page = new PageAdmin();
+	
+	$page->setTpl("order-status", [
+		"order"=>$order->getValues(),
+		"status"=>OrderStatus::listAll(),
+		"msgSuccess"=>Order::getSuccess(),
+		"msgError"=>Order::getError()
+	]);
+});
+
+$app->post("/admin/orders/:idorder/status", function($idorder){
+	
+	User::verifyLogin();
+	
+	if (!isset($_POST['idstatus']) || !(int)$_POST['idstatus'] > 0){
+		Order::setError("Informe o status atual");
+		header("Location: /admin/orders/".$idorder."/status");
+		exit;
+	}
+		
+	$order = new Order();
+			
+	$order->get((int)$idorder);
+	
+	$order->setidstatus((int)$idorder);
+	
+	$order->save();
+	
+	Order::setSuccess("Status atualizado");
+	
+	header("Location: /admin/orders/".$idorder."/status");
+	exit;
+	
+	
+});
 
 $app->get("/admin/orders/:idorder/delete", function($idorder){
 	
@@ -31,7 +76,7 @@ $app->get("/admin/orders/:idorder", function($idorder){
 	$page = new PageAdmin();	
 		
 	$page->setTpl("order", [
-		"orders"=>$order->getValues(),
+		"order"=>$order->getValues(),
 		"cart"=>$cart->getValues(),
 		"products"=>$cart->getProducts()
 	]);
