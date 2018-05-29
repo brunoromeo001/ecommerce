350522af40691573b0eb9b43ce9f8c0bd4281863
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2018-05-29 14:09:04 -0300

    Integração Pagseguro e Paypal

--- views-cache/categories-menu.2a1723b99dcdf518ce3203ca2a152eac.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/6">Androids</a></li><li><a href="/categories/5">Apple</a></li><li><a href="/categories/8">Motorola</a></li><li><a href="/categories/7">Samsung</a></li><li><a href="/categories/9">Sony</a></li>
