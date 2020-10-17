6166d83883704a0638eb5deed5679eada4fa7545
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2020-10-17 19:45:06 -0300

    Classe Address

--- vendor/hcodebr/php-classes/src/PagSeguro/Address.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Address.php
@@ -13,6 +13,116 @@ class Address{
   private $state;  
   private $country;
 
+  public function __construct(
+    string $street, 
+    string $number, 
+    string $complement, 
+    string $district, 
+    string $postalCode, 
+    string $city, 
+    string $state, 
+    string $country
+  )
+  {
+
+    if(!$street)
+    {
+
+      throw new Exception("Informe o endereço");
+
+    }
+
+    if(!$number)
+    {
+
+      throw new Exception("Informe o número.");
+
+    }
+    
+
+    if(!$district)
+    {
+
+      throw new Exception("Informe o bairro.");
+
+    }
+
+    if(!$postalCode)
+    {
+
+      throw new Exception("Informe o CEP.");
+
+    }
+
+    if(!$city)
+    {
+
+      throw new Exception("Informe a cidade.");
+
+    }
+
+    if(!$state)
+    {
+
+      throw new Exception("Informe o estado.");
+
+    }
+
+    if(!$country)
+    {
+
+      throw new Exception("Informe o país.");
+
+    }
+    
+
+    $this->$street = $street;
+    $this->$number = $number;
+    $this->$complement = $complement;
+    $this->$district = $district;
+    $this->$postalCode = $postalCode;
+    $this->$city = $city;
+    $this->$state = $state;
+    $this->$country = $country;
+
+  }
+
+  public function getDOMElement($node = "address"):DOMElement
+  {
+
+    $dom = new DOMDocument();
+
+    $address = $dom->createElement($node);
+    $address = $dom->appendChild($address);
+
+    $street = $dom->createElement("street", $this->$street);
+    $street = $address->appendChild($street);
+
+    $number = $dom->createElement("number", $this->number);
+    $number = $address->appendChild($number);
+
+    $complement = $dom->createElement("complement", $this->complement);
+    $complement = $address->appendChild($complement);
+
+    $district = $dom->createElement("district", $this->district);
+    $district = $address->appendChild($district);
+
+    $postalCode = $dom->createElement("postalCode", $this->postalCode);
+    $postalCode = $address->appendChild($postalCode);
+
+    $city = $dom->createElement("city", $this->city);
+    $city = $address->appendChild($city);
+
+    $state = $dom->createElement("state", $this->state);
+    $state = $address->appendChild($state);
+
+    $country = $dom->createElement("country", $this->country);
+    $country = $address->appendChild($country);
+
+    return $address;
+
+  }
+
 };
 
 ?>
--- vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
@@ -7,6 +7,46 @@ class Phone{
   private $areaCode;
   private $number;
 
+  public function __construct(int $areaCode, int $number)
+  {
+
+    if(!$areaCode || $areaCode < 11 || $areaCode > 99 )
+    {
+
+      throw new Exception("Informe o DDD do telefone.");
+
+    }
+
+    if(!$number || strlen($number) < 9 || $number > 10 )
+    {
+
+      throw new Exception("Informe o número do telefone.");
+
+    }
+
+    $this->$areaCode = $areaCode;
+    $this->$number = $number;
+
+  }
+
+  public function getDOMElement():DOMElement
+  {
+
+    $dom = new DOMDocument();
+
+    $phone = $dom->createElement("phone");
+    $phone = $dom->appendChild($phone);
+
+    $areaCode = $dom->createElement("areaCode", $this->areaCode);
+    $areaCode = $document->appendChild($areaCode);
+
+    $phone = $dom->createElement("number", $this->number);
+    $phone = $phone->appendChild($number);
+
+    return $phone;
+
+  }
+
 };
 
 ?>
