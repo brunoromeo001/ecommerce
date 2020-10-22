74fa7514b4fcb7496c4757371e1a5e3f6a230098
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2020-10-22 17:57:40 -0300

    Item

--- vendor/hcodebr/php-classes/src/PagSeguro/Item.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Item.php
@@ -9,6 +9,61 @@ class Item{
   private $amount;
   private $quantity;
 
+  public function __construct(
+    int $id,
+    string $description,
+    float $amount,
+    int $quantity
+  )
+  {
+
+    if (!$id || !$id > 0){
+
+      throw new Exception("Informe o ID o item.");
+    }
+
+    if (!$description){
+
+      throw new Exception("Informe a descrição do item.");
+    }
+
+    if (!$amount || !$amount > 0){
+
+      throw new Exception("Informe o valor do item.");
+    }
+    if (!$quantity || !$quantity > 0){
+
+      throw new Exception("Informe a quantidade do item.");
+    }   
+
+    $this->$id = $id;
+    $this->$description = $description;
+    $this->$amount = $amount;
+    $this->$quantity = $quantity;
+  }
+
+  public function getDOMElement():DOMElement
+  {
+
+    $dom = new DOMDocument();
+
+    $item = $dom->createElement("item");
+    $item = $dom->appendChild($item);
+
+    $amount = $dom->createElement("amount", number_format($this->amount, 2, '.', ''));
+    $amount = $shipping->appendChild($amount);
+
+    $quantity = $dom->createElement("quantity", $this->quantity);
+    $quantity = $shipping->appendChild($quantity);
+
+    $description = $dom->createElement("description", $this->description);
+    $description = $shipping->appendChild($description);
+   
+    return $installment;
+
+  }
+
+
 };
 
 ?>
